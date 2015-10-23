3e7a1719b323ca6310c3b4624ffdfe1ac70333ac
hrkim3468 <hrkim3468@gmail.com>
2015-10-23 15:56:31 +0900

    기본 디렉토리 구성

---
+++ bingo-server/src/main/java/com/navercorp/DefaultSpringbootWebGradleApplication.java
@@ -0,0 +1,12 @@
+package com.navercorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultSpringbootWebGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultSpringbootWebGradleApplication.class, args);
+    }
+}
