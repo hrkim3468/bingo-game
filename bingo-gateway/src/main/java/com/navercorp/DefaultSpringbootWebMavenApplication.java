c0ee139802b851d913cce3dd49cbd3dc16d21793
hrkim3468 <hrkim3468@gmail.com>
2015-10-28 18:28:54 +0900

    Gateway 프로젝트 생성

---
+++ bingo-gateway/src/main/java/com/navercorp/DefaultSpringbootWebMavenApplication.java
@@ -0,0 +1,12 @@
+package com.navercorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultSpringbootWebMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultSpringbootWebMavenApplication.class, args);
+    }
+}
